17b5d54ce0f213a62d81c286b7087c9b38175e82
codecurser <aryansharma35x@gmail.com>
2026-01-31 22:28:27 +0530

    Implement pangram check in apanagram.java
    Added functionality to check if a string is a pangram.

---
+++ apanagram.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt(); 
+        String s = sc.next();
+        
+        s = s.toLowerCase();    // make case-insensitive
+        
+        Set<Character> set = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            set.add(c);
+        }
+        
+        if (set.size() == 26) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
